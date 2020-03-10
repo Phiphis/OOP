c1b8559805607de19548e3be5ff83cbe6f461531
Phiphi <s3817921@rmit.edu.vn>
2020-03-10 13:22:27 +0700

    displaying table

--- src/wk1/displayTable.java
+++ src/wk1/displayTable.java
@@ -1,11 +1,14 @@
 package wk1;
 
+import java.util.Formatter;
+
 public class displayTable {
     public static void main(String[] args) {
         //System.out.printf("Hello %d%n, miles*1.609);
-        double[] miles = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65};
-        for (double mile : miles) {
-            System.out.printf(mile + "      %d%n", mile*1.609);
+        int[] miles = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println("Miles   Kilometer   |   Kilometers  Miles");
+        for (int mile : miles) {
+            System.out.printf(" %d \t\t %.2f \t\t|\t\t%d \t\t%.2f%n", mile, mile*1.609, 20+(mile-1)*5, (20+(mile-1)*5)/1.609);
         }
     }
 }
